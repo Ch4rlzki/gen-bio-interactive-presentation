48ebd2d748989eb30501c02c9d4b21a9f95a2408
Ch4rlzki <133903363+Ch4rlzki@users.noreply.github.com>
2023-09-26 06:44:02 -0700

    Fixes v15

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
 import Home from "./pages/Home.js";
 import Diseases from "./pages/Diseases.js";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path="/" element={<Home/>}></Route>
         <Route path="/diseases" element={<Diseases/>}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
